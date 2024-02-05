cfe103a24859feff59163c8a65f28634b6cfc6f7
Skyler Kinney <Skylerjkinney@gmail.com>
2024-02-05 15:17:50 -0700

    project initialized

---
+++ Models/Account.cs
@@ -0,0 +1,9 @@
+namespace catNet.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
